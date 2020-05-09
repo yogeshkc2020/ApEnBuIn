b729e1b9ca69179709d0beecea21d50ef4717207
Yogesh Kumar Chawla <yogesh@MacBook-Pro-di-Yogesh.local>
2020-05-10 01:12:06 +0200

    Added list of books and navbar php files

---
+++ src/listofbooks.php
@@ -0,0 +1,25 @@
+<?php
+  include "navbar.php";
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <title>List Of Books</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+  <h2 style="text-align: center;">List Of Books</h2>
+  <table class='table table-border table-hover'>
+    <tr style='background-color: #19d7e0;'>
+      <th>ID</th>
+      <th>ISBN</th>
+      <th>Book Title</th>
+      <th>Author Name</th>
+      <th>Edition</th>
+      <th>Status</th>
+      <th>Quantity</th>
+    </tr>
+  </table>
+</body>
+</html>
