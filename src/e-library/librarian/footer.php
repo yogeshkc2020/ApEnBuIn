01e92e428f1d075db4afa39d0030fc5a4e04bbff
Yogesh Kumar Chawla <yogesh@MacBook-Pro-di-Yogesh.local>
2020-05-19 08:30:45 +0200

    Changes

---
+++ src/e-library/librarian/footer.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<style>
+footer {
+height: 82px;
+width: 100%;
+background-color: #19d7e0;
+}
+</style>
+</head>
+<body>
+    <footer>
+        <p style="color: black; text-align: center;"><br>
+        Email:&nbsp e-library@gmail.it <br><br>
+        Mobile:&nbsp &nbsp +393471356452
+		</p>
+	</footer>
+</body>
+</html>
